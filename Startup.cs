1c2b147401b4b2a82e18c6d432cb5867e6b7f1be
Abdur Rahim <36808113+webfrontendbd@users.noreply.github.com>
2018-11-07 12:56:47 +0600

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GalleriaSupermarket.Startup))]
+namespace GalleriaSupermarket
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
